1f8ab82b0956c2bdf27ec3075cdad1d53509adc7
Owen Valentinus <owenwijaya89@gmail.com>
2021-03-25 14:29:34 +0700

    foo

--- discord_api.py
+++ discord_api.py
@@ -4,54 +4,53 @@ import re
 from time import sleep as s
 payload = {}
 
-def search_guild_all_messages():
-    #Getting the infos needed
-    global channel_id, search_url
-    guild, channel_id = ' '.join(re.findall('(\d+)',input('Guild url: '))).split()
-    search_url = f'https://discord.com/api/v6/guilds/{guild}/messages/search?channel_id={channel_id}'
-
-def search_dm_all_messages():
-    #Getting the infos needed
-    global channel_id, search_url
-    channel_id = ''.join(re.findall('/?(\d+)', input('Dm url: ')))
-    search_url = f"https://discord.com/api/v6/channels/{channel_id}/messages/search?author_id="
-
-def download_all_messages():
-    response = requests.get(search_url, headers=headers, data=payload)
-    data = json.loads(response.text)
-    with open('./raw.json', 'w', encoding='utf-8') as file:
-        file.write(json.dumps(data))
-        # file.write(json.dumps([x[0]['content'] for x in data['messages']][::-1]))
-
-def get_messages_id():
-    response = requests.get(search_url, headers=headers, data=payload)
-    data = json.loads(response.text)
-    return [x[0]['id'] for x in data['messages']]
-
-def _delete():
-    messages_url = f"https://discord.com/api/v6/channels/{channel_id}/messages/"
-    while True:
-        for x in get_messages_id():
-            message_url = messages_url + x
-            s(1)
-            responses = requests.delete(message_url, headers=headers, data=payload)
-            print(responses.status_code)
-        get_messages_id()
-
-def send_message():
-    response = requests.post('https://discord.com/api/v8/channels/783304248476565524/messages', headers=headers, data={'content': "since you are offline, i'll just send the goodbye from your discord app", 'tts': 'false'})
-
-a = input('Guild or dm?: ')
-if a.lower() == 'g':
-    search_guild_all_messages()
-
-else:
-    search_dm_all_messages()
-
-#Listing all the messages sent by the author
-
 headers = {
-    'authorization': 'Mjc0NjM5NDA1MjM3NjY1Nzky.YFcbSA.uNrWdQfqrHV9wvGGT5Ltggo5OJ0',
-}
+        'authorization': 'Nzc4NTM1ODEzNDM2MTQ1Njk2.YDC2ww.vrHd-dDQX1XDweKz48_3IRVzdvg'
+    }
 
-download_all_messages()
+def get_ids():
+    guild_channel = input('Input the url here: ')
+    return re.findall('\d+', guild_channel)
+
+def get_search_url(guild_channel_ids):
+    #it's a guild
+    if len(guild_channel_ids) > 1:
+        guild_id = guild_channel_ids[-2]
+        channel_id = guild_channel_ids[-1]
+        return f'https://discord.com/api/v6/guilds/{guild_id}/messages/search?channel_id={channel_id}'
+    elif len(guild_channel_ids) == 1:
+        #it's a dm message
+        channel_id = ''.join(guild_channel_ids)
+        return f'https://discord.com/api/v6/channels/{channel_id}/messages/search?'
+
+def get_author_id():
+    author_id = input('Specify the author id? (press enter to skip): ')
+    return author_id
+
+def get_author_search_url(author_id, search_url):
+    if author_id == None:
+        return f"{search_url}author_id=778535813436145696&sort_by=timestamp&sort_order=desc&offset=0"
+
+def get_total_messages(author_search_url):
+    r = requests.get(author_search_url, headers=headers)
+    data = r.json()
+    return data['total_results']
+
+def get_messages_id(total_messages, author_search_url):
+    for x in range(0, total_messages, 25):
+        r = requests.get(author_search_url, headers = headers)
+        print(r.json())
+        with open('raw.json', 'r+', encoding='utf-8') as file:
+            file.write(json.dumps(r.json()))
+
+def main():
+    ids = get_ids()
+    search_url = get_search_url(ids)
+    author_id = get_author_id()
+    author_search_url = get_author_search_url(author_id, search_url)
+    # total_messages = get_total_messages(author_search_url)
+    # messages_id = get_messages_id(total_messages, author_search_url)
+    # print(messages_id)
+
+if __name__ == '__main__':
+    main()
