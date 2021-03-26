1c66a9611864dbe30a99eb91d390b9afc3b0f793
Owen Valentinus <owenwijaya89@gmail.com>
2021-03-26 23:44:58 +0700

    Finished the delete bot

--- discord_api.py
+++ discord_api.py
@@ -2,13 +2,15 @@ import requests
 import json
 import re
 from time import sleep as s
+import math
 payload = {}
 
 headers = {
-        'authorization': 'Nzc4NTM1ODEzNDM2MTQ1Njk2.YDC2ww.vrHd-dDQX1XDweKz48_3IRVzdvg'
+        'authorization': 'Nzc4NTM1ODEzNDM2MTQ1Njk2.YDC2ww.vrHd-dDQX1XDweKz48_3IRVzdvg',
     }
 
-def get_ids():
+def get_channel_id():
+    #parsing the channel id
     guild_channel = input('Input the url here: ')
     return re.findall('\d+', guild_channel)
 
@@ -21,36 +23,47 @@ def get_search_url(guild_channel_ids):
     elif len(guild_channel_ids) == 1:
         #it's a dm message
         channel_id = ''.join(guild_channel_ids)
-        return f'https://discord.com/api/v6/channels/{channel_id}/messages/search?'
+        return f'https://discord.com/api/v6/channels/{channel_id}/messages'
 
 def get_author_id():
+    #get author id, returning none if not specified
     author_id = input('Specify the author id? (press enter to skip): ')
     return author_id
 
 def get_author_search_url(author_id, search_url):
-    if author_id == None:
-        return f"{search_url}author_id=778535813436145696&sort_by=timestamp&sort_order=desc&offset=0"
+    #get the search url combined with the id of the author
+    return f"{search_url}/search?author_id={author_id}&sort_by=timestamp&sort_order=desc&offset=0" if len(author_id) > 0 else f'{search_url}/search?sort_by=timestamp&sort_order=desc&offset=0'
 
 def get_total_messages(author_search_url):
+    #get total messages of the channel
     r = requests.get(author_search_url, headers=headers)
     data = r.json()
     return data['total_results']
 
-def get_messages_id(total_messages, author_search_url):
-    for x in range(0, total_messages, 25):
-        r = requests.get(author_search_url, headers = headers)
-        print(r.json())
-        with open('raw.json', 'r+', encoding='utf-8') as file:
-            file.write(json.dumps(r.json()))
+def get_message_id(message_data):
+    return [message_info['id'] for message_infos in message_data['messages'] for message_info in message_infos]
+
+def delete(message_id, search_url):
+    for x in message_id:
+        r = requests.delete(search_url + '/' + x, headers=headers)
+        s(1)
+        print(r.status_code)
+
+def recurse_delete(total_messages, author_search_url, search_url):
+    loop = math.ceil(total_messages/25)
+    for x in range(0, loop):
+        r = requests.get(author_search_url, headers=headers)
+        data = r.json()
+        message_id = get_message_id(data)
+        delete(message_id, search_url)
 
 def main():
-    ids = get_ids()
-    search_url = get_search_url(ids)
+    channel_ids = get_channel_id()
+    search_url = get_search_url(channel_ids)
     author_id = get_author_id()
     author_search_url = get_author_search_url(author_id, search_url)
-    # total_messages = get_total_messages(author_search_url)
-    # messages_id = get_messages_id(total_messages, author_search_url)
-    # print(messages_id)
+    total_messages = get_total_messages(author_search_url)
+    recurse_delete(total_messages, author_search_url, search_url)
 
 if __name__ == '__main__':
     main()
