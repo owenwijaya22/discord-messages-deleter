92341c70ea6e5dfc44f333cfa0c9a4d0958b281e
unknown <owenwijaya89@gmail.com>
2021-12-26 14:28:13 +0700

    Added second account for the booster

--- src/stem_thank_you_booster/boost.py
+++ src/stem_thank_you_booster/boost.py
@@ -1,4 +1,12 @@
 import requests
+import random
+
+
+def choose_token():
+    token = random.choice(
+        "ODEzNzAyMDg1NTA5Nzc1Mzcw.YcAm7w.ie_QO2nhvWi5i5R6XqLq164JKZo",
+        "Nzc4NTM1ODEzNDM2MTQ1Njk2.YcgVlw.6Rr0qHMUHWdI0CjedkvdVA9Dbu4")
+    return token
 
 
 def choose_url():
@@ -12,11 +20,10 @@ def choose_url():
 
 def main():
     url = choose_url()
+    token = choose_token()
     payload = "{\"content\":\"<@!300506208832585728> thank you\",\"nonce\":\"923027088506617856\",\"tts\":false}"
     headers = {
-        'user-agent': "vscode-restclient",
-        'authorization':
-        "ODEzNzAyMDg1NTA5Nzc1Mzcw.YcAm7w.ie_QO2nhvWi5i5R6XqLq164JKZo",
+        'authorization': token,
         'content-type': "application/json"
     }
     response = requests.request("POST", url, data=payload, headers=headers)
