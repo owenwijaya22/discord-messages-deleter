43ea040bd9d5cdcc1a5f93ba02fcc9aa734614b9
unknown <owenwijaya89@gmail.com>
2021-12-27 09:28:25 +0700

    Changed channels and the file's name

--- src/stem_thank_you_booster/1.py
+++ src/stem_thank_you_booster/1.py
@@ -3,16 +3,17 @@ import random
 
 
 def choose_token():
-    token = random.choice(
+    token = random.choice([
         "ODEzNzAyMDg1NTA5Nzc1Mzcw.YcAm7w.ie_QO2nhvWi5i5R6XqLq164JKZo",
-        "Nzc4NTM1ODEzNDM2MTQ1Njk2.YcgVlw.6Rr0qHMUHWdI0CjedkvdVA9Dbu4")
+        "Nzc4NTM1ODEzNDM2MTQ1Njk2.YcgVlw.6Rr0qHMUHWdI0CjedkvdVA9Dbu4"
+    ])
     return token
 
 
 def choose_url():
     subject = input('Math (m) or Physics (p): ')
     if subject == 'm':
-        url = "https://discord.com/api/v9/channels/641351291343208448/messages"
+        url = "https://discord.com/api/v9/channels/641351235215294486/messages"
     elif subject == 'p':
         url = "https://discord.com/api/v9/channels/536995799859724309/messages"
     return url
@@ -22,10 +23,7 @@ def main():
     url = choose_url()
     token = choose_token()
     payload = "{\"content\":\"<@!300506208832585728> thank you\",\"nonce\":\"923027088506617856\",\"tts\":false}"
-    headers = {
-        'authorization': token,
-        'content-type': "application/json"
-    }
+    headers = {'authorization': token, 'content-type': "application/json"}
     response = requests.request("POST", url, data=payload, headers=headers)
     print(response.text)
 
